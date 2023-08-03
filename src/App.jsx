3d1c89bffbad9997d50b0d70fb8c04962784957f
“Raizo-n” <sanpincha0826@gmail.com>
2023-08-02 23:13:05 -0700

    add: fetch api and .env file

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import { useState } from 'react'
-import './App.css'
-import Form from './components/Form'
-import Title from './components/Title'
+import { useState } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import Title from "./components/Title";
+import axios from "axios";
+import.meta.env.VITE_REACT_APP_ACCESS_KEY;
 
 function App() {
-  const [word, setWord] = useState('cat');
-
+  const [word, setWord] = useState("cat");
+  const getPhotoData = (e) => {
+    e.preventDefault();
+    axios
+      .get(`https://api.unsplash.com/search/photos?query=${word}&client_id=${import.meta.env.VITE_REACT_APP_ACCESS_KEY
+    }`)
+      .then((res) => console.log(res));
+  };
   return (
     <div className="App">
-      <Title/>
-      <Form setWord={setWord}/>
-      {word}
+      <Title />
+      <Form setWord={setWord} getPhotoData={getPhotoData} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
